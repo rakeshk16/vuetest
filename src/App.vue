1f76828dfe5e05284262fa711ba6340e4105dbae
rakeshk16 <rakeshntak@gmail.com>
2020-12-08 13:43:52 +0530

    Components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Todos from "./components/Todos";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Todos,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "name1",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "name2",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "name3",
+          completed: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-bind:class="{ 'is-complete': todo.completed }">
+    <p v-on:click="toggle">{{ todo.title }}</p>
+    <button @click="$emit('del-todo',todo.id)">X</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDoItem",
+  props: ["todo"],
+  methods: {
+    toggle() {
+      this.todo.completed = !this.todo.completed;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.is-complete {
+  text-decoration: line-through;
+}
+</style>
